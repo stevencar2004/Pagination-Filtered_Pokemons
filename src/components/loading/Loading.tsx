78f74ffd814e044b67e8cbb0affa5e5ee66da828
steven cardona <you@example.com>
2021-11-27 00:57:05 -0500

    V 1.0 component loading

---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,8 @@
+export const Loading = () => {
+	return (
+		<div className="d-flex align-items-center">
+			<strong className="fs-3">Upload Pokemons...</strong>
+			<div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+		</div>
+	);
+};
