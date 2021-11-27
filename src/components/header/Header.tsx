aab0f0846acda1a3cdce289b04e9275c2da892e8
steven cardona <you@example.com>
2021-11-27 00:56:23 -0500

    V 1.0 component header of web page

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+	return (
+		<div className="row p-3 bg-warning shadow-sm">
+			<div className="col">
+				<h2>
+					<i className="fab fa-superpowers"></i>
+					Poke<b>React</b>
+				</h2>
+			</div>
+		</div>
+	);
+};
