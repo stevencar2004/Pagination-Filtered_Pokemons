5a144b8ce17d76e53f4bc99f7b03c1d57b7cfce2
steven cardona <you@example.com>
2021-11-27 00:52:52 -0500

    add components, context  and class for bootstrap

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,58 @@
+import { useContext } from "react";
+import { pokeContext } from "../context/Context";
+
 import { Loading } from "../components/loading/Loading";
 import { Header } from "../components/header/Header";
-import { usePokemon } from "../hooks/usePokemon";
+import { SearchField } from "../components/Search/SearchField";
+import { SelectNumPoke } from "../components/numPokeItems/SelectNumPoke";
+import { ControlsPage } from "../components/ControlsPages/ControlsPage";
+import { PokeListItems } from "../components/PokeItem/PokeListItems";
 
 export const Home = () => {
-	const { isLoading, pokemons } = usePokemon();
+	const { isLoading, setIsViewGrid } = useContext(pokeContext);
 
 	return (
-		<div>
+		<>
 			<Header />
 
-			<table>
-				<thead>
-					<tr>
-						<td>ID</td>
-						<td>Nombre</td>
-						<td>Imagen</td>
-					</tr>
-				</thead>
+			<div className="row mt-5 m-3 align-items-center" id="menuFilter">
+				<div className=" col-md-8">
+					<SearchField />
+				</div>
 
-				<tbody>
-					{pokemons.map(({ id, name, imgURL }) => (
-						<tr key={id}>
-							<td>{id}</td>
-							<td>{name}</td>
-							<td>
-								<img src={imgURL} alt={name} width="100" />
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
+				<div className="col d-flex justify-content-center gap-1">
+					<button
+						className="btn btn-outline-secondary"
+						onClick={() => {
+							setIsViewGrid(true);
+						}}
+					>
+						<i className="fas fa-th"></i>
+					</button>
 
-			{isLoading && <Loading />}
-		</div>
+					<button
+						className="btn btn-outline-secondary"
+						onClick={() => {
+							setIsViewGrid(false);
+						}}
+					>
+						<i className="fas fa-th-list"></i>
+					</button>
+				</div>
+
+				<div className="col">
+					<SelectNumPoke />
+				</div>
+			</div>
+
+			<main className="m-5">
+				{isLoading && <Loading />}
+
+				<section>
+					<PokeListItems />
+					<ControlsPage />
+				</section>
+			</main>
+		</>
 	);
 };
